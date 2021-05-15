fc3c138ff99c53ff7a44751c59533f1a34e13e53
Gabriel Morales <gabbomorales1@gmail.com>
2021-05-15 04:24:54 -0500

    Button component added

---
+++ server/client/components/atoms/button/index.tsx
@@ -0,0 +1,15 @@
+import { StyledButton } from './styles'
+
+interface ButtonProps {
+  onClick: any
+}
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  return (
+    <>
+      <StyledButton>{children}</StyledButton>
+    </>
+  )
+}
+
+export default Button
