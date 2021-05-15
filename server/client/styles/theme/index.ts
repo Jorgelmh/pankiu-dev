c9880c2089c3f47de9fb87062a88483378e5c9ff
Gabriel Morales <gabbomorales1@gmail.com>
2021-05-15 06:24:50 -0500

    app setted up

---
+++ server/client/styles/theme/index.ts
@@ -0,0 +1,13 @@
+const colors = {
+  green: '#33d06c',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#999',
+  red: '#FB6767',
+  yellow: '#F2C44C',
+  blue: '#4fb8e1',
+  bronze: '#aca976',
+  auto: 'auto',
+}
+
+export default colors
