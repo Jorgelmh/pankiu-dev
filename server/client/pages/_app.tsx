39158c85e3660426ba131e538469ec790d145b0e
Gabriel Morales <gabbomorales1@gmail.com>
2021-05-15 19:20:00 -0500

    added theme to the project

--- server/client/pages/_app.tsx
+++ server/client/pages/_app.tsx
@@ -1,17 +1,13 @@
 import Layout from '@templates/layout'
 import GlobalStyles from '@styles/global'
-import { ThemeProvider } from 'styled-components'
-
-import Theme from '@styles/theme'
+import '@styles/theme/hackaton.css'
 import { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <GlobalStyles />
-      <ThemeProvider theme={Theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </Layout>
   )
 }
