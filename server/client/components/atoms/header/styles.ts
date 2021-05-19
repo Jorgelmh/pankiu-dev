4da79e5ae3c4ff00bffee39146c66882594acb55
Gabriel Morales <gabbomorales1@gmail.com>
2021-05-19 01:15:23 -0500

    Menu added

--- server/client/components/atoms/header/styles.ts
+++ server/client/components/atoms/header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const StyledHeader = styled.div`
-  width: 414px;
+  width: 100%;
   height: 225px;
   background: #6e6be8;
   border-radius: 0 0 40px 40px;
--- server/client/components/organisms/menu/index.tsx
+++ server/client/components/organisms/menu/index.tsx
@@ -1,9 +1,15 @@
 import { StyledContainer } from './styles'
+import IconText from '@molecules/icontext'
 
 const Menu: React.FC = () => {
   return (
     <>
-      <StyledContainer>a</StyledContainer>
+      <StyledContainer>
+        <IconText>Jusus</IconText>
+        <IconText>Judas</IconText>
+        <IconText>Mateo</IconText>
+        <IconText>SapoPuto</IconText>
+      </StyledContainer>
     </>
   )
 }
