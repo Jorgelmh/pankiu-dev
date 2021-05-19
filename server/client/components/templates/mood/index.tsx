cae0b35e86e4bb23ab34b89bb3bebf754430c8f0
Gabriel Morales <gabbomorales1@gmail.com>
2021-05-19 03:23:54 -0500

    Added rooting to almost all the pages

---
+++ server/client/components/templates/mood/index.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+  return <></>
+}
+
+export default App
---
+++ server/client/pages/chats/index.tsx
@@ -0,0 +1,11 @@
+import App from '@components/templates/chats'
+
+const Chat: React.FC = () => {
+  return (
+    <>
+      <App />
+    </>
+  )
+}
+
+export default Chat
