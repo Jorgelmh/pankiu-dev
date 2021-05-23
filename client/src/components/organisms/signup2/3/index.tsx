0c5cd1795e1e85588d707c982034321a8f22809d
Mang0 <82917547+TMang0@users.noreply.github.com>
2021-05-22 19:17:27 -0500

    fixed layout of signup page

---
+++ client/src/components/organisms/signup2/3/index.tsx
@@ -0,0 +1,11 @@
+import Text from "components/atoms/text"
+import Title from "components/atoms/title"
+
+const SignupText: React.FC = () => {
+  return (
+    <div style={{display:'flex', justifyContent:'center'}}>
+      <Title>Sign Up</Title>
+    </div>
+  )}
+
+export default SignupText
--- client/src/components/templates/signup/index.tsx
+++ client/src/components/templates/signup/index.tsx
@@ -2,23 +2,18 @@ import SignUpForm from "components/organisms/signupform";
 import TopBarSignup from "components/organisms/signup2"
 import Text from "components/atoms/text";
 import { Button } from "@progress/kendo-react-buttons";
+import Botsignup from "components/organisms/signup2/2";
+import SignupText from "components/organisms/signup2/3";
 
 const App: React.FC = () => {
   return (
     <>
     <TopBarSignup/>
+    <SignupText/>
     <div style={{display:'flex', justifyContent:'center', height:'360px'}}>
       <SignUpForm/>
       </div>
-      <div style={{display:'flex', justifyContent:'center', margin:'1rem auto'}}>
-      <Text>Or</Text>
-      </div>
-      <div style={{margin:'2rem auto',display:'flex', justifyContent:'center'}}>
-      <Button>Login</Button>
-      </div>
-      <div>
-      <a style={{display:'flex', justifyContent:'center',margin:'2rem auto'}} href="#">Forgot your password?</a>
-      </div>
+      <Botsignup/>
     </>
 
   );
